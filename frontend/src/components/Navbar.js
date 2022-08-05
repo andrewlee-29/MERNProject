2e936f077f44370c5cbb7d81e510473fa8e43b55
andrewlee10 <andrew.lee1@toasttab.com>
2022-08-05 15:29:20 -0400

    frontend update

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,12 @@
+import {Link} from 'react-router-dom'
+const Navbar =()=>{
+    return (
+        <div className="container">
+            <Link to="/">
+            <h2>workout buddy</h2>
+            </Link>
+        </div>
+    )
+}
+
+export default Navbar
